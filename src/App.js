a0290ba82f77b57df3ccb8987f5bf208ba844c08
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-04-12 03:54:58 +0530

    added login component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import Login from './components/pages/Login'
 
 function App() {
   return (
     <div className="App">
-      <Main/>
-      <Navbar/>
+      {/* <Main/> */}
+      {/* <Navbar/> */}
+      <Login />
     </div>
   );
 }
