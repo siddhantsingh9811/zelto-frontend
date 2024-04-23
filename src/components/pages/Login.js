c7dec26d83b9f5edac61f4caefa237a2e16b6b7b
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-04-23 22:32:29 +0530

    auth completed with token

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,87 +1,154 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook from React Router
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
+  const navigate = useNavigate(); // Access the navigate function from useNavigate
 
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    try {
+      const response = await axios.post("http://localhost:5000/api/login", {
+        username,
+        password,
+      });
+
+      // Check if login was successful
+      if (response.status === 200) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        setShowSuccessMessage(true);
+        setUsername("");
+        setPassword("");
+        
+        // Redirect to "/home" endpoint
+        navigate("/");
+      } else {
+        console.log("Error in login");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
-  
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        
-          <h2 className="mt-10 text-center text-2xl leading-9 tracking-tight text-gray-900">
-            Sign in to your Zelto account
-          </h2>
-        </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl leading-9 tracking-tight text-gray-900">
+          Sign in to your Zelto account
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email 
-              </label>
-              <div className="mt-2 flex">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div className="mt-2 flex">
               <img src="frame.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
-                <input
-                  id="email"
-                  value={email}
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
+              <input
+                id="username"
+                value={username}
+                name="username"
+                type="username"
+                autoComplete="username"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
+          </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href=" " className="font-semibold text-[#755CEC] hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2 flex">
-              <img src="key.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
-                <input
-                  id="password"
-                  value={password}
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-3xl bg-[#755CEC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Sign in
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href=" "
+                  className="font-semibold text-[#755CEC] hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2 flex">
+              <img src="key.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
+              <input
+                id="password"
+                value={password}
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-3xl bg-[#755CEC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        {showSuccessMessage && (
+          <div className="mt-4 mx-auto w-full max-w-sm">
+            <div
+              className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-md relative"
+              role="alert"
+            >
+              <span className="block sm:inline">Logged In</span>
+              <button
+                onClick={() => setShowSuccessMessage(false)}
+                className="absolute top-0 bottom-0 right-0 px-4 py-3"
+              >
+                <svg
+                  className="fill-current h-6 w-6 text-green-500"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path
+                    fillRule="evenodd"
+                    d="M14.354 5.646a.5.5 0 0 0-.708 0L10 9.293 5.354 5.646a.5.5 0 1 0-.708.708L9.293 10l-4.647 4.646a.5.5 0 1 0 .708.708L10 10.707l4.646 4.647a.5.5 0 0 0 .708-.708L10.707 10l4.647-4.646a.5.5 0 0 0 0-.708z"
+                  />
+                </svg>
               </button>
             </div>
-          </form>
+          </div>
+        )}
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Sign Up
-            </a>
-          </p>
-        </div>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <a
+            href="/signup"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign Up
+          </a>
+        </p>
       </div>
-  );}
-export default Login
+    </div>
+  );
+}
+export default Login;
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,103 +1,149 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import axios from "axios";
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [number, setNumber] = useState("");
-    
-    return (
-        
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            
-              <h2 className="mt-10 text-center text-2xl leading-9 tracking-tight  text-gray-900">
-                Create Zelto Account
-              </h2>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Make POST request using Axios
+    try {
+      await axios.post("http://localhost:5000/api/register", {
+        username,
+        email,
+        password
+      });
+
+      // Show success message
+      setShowSuccessMessage(true);
+
+      // Reset form fields
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      // Handle error
+      console.error("Error:", error);
+    }
+  };
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl leading-9 tracking-tight  text-gray-900">
+          Create Zelto Account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" onSubmit={handleSubmit}>  
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div className="mt-2 flex">
+              <img src="frame.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
+              <input
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                name="username"
+                type="username"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
-    
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                    Email 
-                  </label>
-                  <div className="mt-2 flex">
-                  <img src="frame.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
-                    <input
-                      id="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                      Password
-                    </label>
-                  </div>
-                  <div className="mt-2 flex">
-                  <img src="key.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
-                    <input
-                      id="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label htmlFor="number" className="block text-sm font-medium leading-6 text-gray-900">
-                    Phone Number 
-                  </label>
-                  <div className="mt-2 flex">
-                  <img src="frame.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
-                    <input
-                      id="number"
-                      value={number}
-                      onChange={(e) => setNumber(e.target.value)}
-                      name="number"
-                      type="number"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                
-                    
-                <div>
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-3xl bg-[#755CEC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Create Account
-                  </button>
-                </div>
-                
-                
-              </form>
-    
-              <p className="mt-10 text-center text-sm text-gray-500">
-                Already a member?{' '}
-                <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                  Sign In
-                </a>
-              </p>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div className="mt-2 flex">
+              <img src="frame.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
+              <input
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
             </div>
           </div>
 
-          
-      );}
-export default SignUp
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2 flex">
+              <img src="key.png" alt="key" className="h-5 w-5 mr-2 mt-2" />
+              <input
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-3xl bg-[#755CEC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+
+        {showSuccessMessage && (
+          <div className="mt-4 mx-auto w-full max-w-sm">
+            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-md relative" role="alert">
+              <span className="block sm:inline">uccessfully registered!</span>
+              <button
+                onClick={() => setShowSuccessMessage(false)}
+                className="absolute top-0 bottom-0 right-0 px-4 py-3"
+              >
+                <svg className="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <title>Close</title>
+                  <path fillRule="evenodd" d="M14.354 5.646a.5.5 0 0 0-.708 0L10 9.293 5.354 5.646a.5.5 0 1 0-.708.708L9.293 10l-4.647 4.646a.5.5 0 1 0 .708.708L10 10.707l4.646 4.647a.5.5 0 0 0 .708-.708L10.707 10l4.647-4.646a.5.5 0 0 0 0-.708z"/>
+                </svg>
+              </button>
+            </div>
+          </div>
+        )}
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already a member?{" "}
+          <a
+            href="/login"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign In
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+export default SignUp;
